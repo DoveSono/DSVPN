a9eca6841ce96b405579ad7121739f50d2af57b5
BDBSAlive6one6 <68449698+DoveSono@users.noreply.github.com>
2025-02-10 12:48:47 +0300

    Update setup_gui.go

--- setup_gui/setup_gui.go
+++ setup_gui/setup_gui.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"DSVPN/gost" // Модуль для генерации ключа и S-блока
+	"DSVPN/gost" // Импорт библиотеки для генерации ключей и S-блока
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
@@ -21,14 +21,14 @@ func openBrowser(url string) {
 		err = exec.Command("xdg-open", url).Start()
 	case "windows":
 		err = exec.Command("cmd", "/C", "start", url).Start()
-	case "darwin": // для macOS
+	case "darwin": // macOS
 		err = exec.Command("open", url).Start()
 	default:
-		log.Println("Неизвестная операционная система, не удается открыть браузер")
+		log.Println("Неизвестная ОС, не удается открыть браузер")
 		return
 	}
 	if err != nil {
-		log.Println("Не удалось открыть браузер:", err)
+		log.Println("Ошибка при открытии браузера:", err)
 	}
 }
 
@@ -36,75 +36,62 @@ func main() {
 	var wg sync.WaitGroup
 	r := gin.Default()
 
-	// Создаем сессию с использованием cookie
+	// Создание сессий
 	store := cookie.NewStore([]byte("secret"))
-	// Время жизни сессии 30 минут
 	store.Options(sessions.Options{
-		MaxAge:   1800, // 30 минут в секундах
+		MaxAge:   1800, // 30 минут
 		HttpOnly: true,
 	})
 	r.Use(sessions.Sessions("mysession", store))
 
-	// Загрузка статических файлов
+	// Подключение статики и HTML
 	r.Static("/web", "./web")
 	r.LoadHTMLGlob("web/*")
 
-	// Директ для главной страницы
+	// Главная страница
 	r.GET("/", func(c *gin.Context) {
-		// Проверка авторизации
 		session := sessions.Default(c)
 		if session.Get("authenticated") != nil {
-			// Если авторизован -  редирект на страницу генерации
 			c.Redirect(http.StatusFound, "/generate")
 			return
 		}
-
-		// Если не авторизован - редирект на страницу авторизации
 		c.HTML(http.StatusOK, "authorization.html", nil)
 	})
 
-	// Директ для авторизации
+	// Авторизация
 	r.POST("/login", func(c *gin.Context) {
 		var form struct {
 			Username string `json:"username" binding:"required"`
 			Password string `json:"password" binding:"required"`
 		}
-
-		// Данные из формы
 		if err := c.ShouldBindJSON(&form); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-
-		// Проверка логина и пароля
 		if form.Username == "admin" && form.Password == "admindsvpn" {
-			// Сохранение сессии
 			session := sessions.Default(c)
 			session.Set("authenticated", true)
 			session.Save()
-
 			c.JSON(http.StatusOK, gin.H{"message": "Авторизация успешна!"})
 		} else {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Неверный логин или пароль"})
 		}
 	})
 
-	// Директ для генерации ключа и S-блока
+	// Страница генерации ключа
 	r.GET("/generate", func(c *gin.Context) {
-		// Проверка авторизации
 		session := sessions.Default(c)
 		if session.Get("authenticated") == nil {
 			c.Redirect(http.StatusFound, "/")
 			return
 		}
-
 		c.HTML(http.StatusOK, "generate.html", gin.H{
 			"key":    "",
 			"sBlock": "",
 		})
 	})
 
-	// Генерация ключа
+	// Генерация ключа и S-блока
 	r.POST("/generate/key", func(c *gin.Context) {
 		key, err := gost.GenerateGostKey()
 		if err != nil {
@@ -114,28 +101,34 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"key": key})
 	})
 
-	// Генерация S-блока
 	r.POST("/generate/sblock", func(c *gin.Context) {
 		sBlock := gost.GenerateGostSBlock()
 		c.JSON(http.StatusOK, gin.H{"sBlock": sBlock})
 	})
 
-	// Добавление горутины с сервером в wait-группу
+	// Страница настройки VPN после генерации
+	r.GET("/setup", func(c *gin.Context) {
+		session := sessions.Default(c)
+		if session.Get("authenticated") == nil {
+			c.Redirect(http.StatusFound, "/")
+			return
+		}
+		c.HTML(http.StatusOK, "servsetup.html", nil)
+	})
+
+	// Запуск сервера
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		err := r.Run(":8080")
 		if err != nil {
-			log.Fatal("Ошибка запуска сервера: ", err)
+			log.Fatal("Ошибка запуска сервера:", err)
 		}
 	}()
 
-	// Задержка, чтобы не было траблов
+	// Открытие браузера через 2 секунды
 	time.Sleep(2 * time.Second)
-
-	// Открытие сайта
 	openBrowser("http://localhost:8080")
 
-	// Ожидание завершения горутины
 	wg.Wait()
 }
